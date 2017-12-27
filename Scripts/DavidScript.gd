3fd69ea66dd8df0bc38c76fd5c47d03d8722b40e
Diego Garrido <de.garrido95@gmail.com>
2017-12-27 04:30:04 -0300

    Hitbox Obstacle

--- Scripts/DavidScript.gd
+++ Scripts/DavidScript.gd
@@ -5,17 +5,20 @@ var input_direction = 0
 var velocity = Vector2()
 var speed_x = 0
 var speed_y = 0
-
+var jumps_until_now = 0
 #HEALTH BAR
 onready var health_bar = get_node("HealthBar")
 
 var health = 100
+var jump_count = 0
 
 const ACCEL = 250
 const DECEL = 500
 const MAX_SPEED = 150
-const JUMP_FORCE = 800
+const JUMP_FORCE = 600
 const GRAVITY = 2000
+const MAX_FALL_SPEED = 1400
+const MAX_JUMP_COUNT = 2
 
 func _ready():
 	set_process_input(true)
@@ -24,8 +27,9 @@ func _ready():
 	pass
 
 func _input(event):
-	if event.is_action_pressed("ui_up"):
+	if jump_count < MAX_JUMP_COUNT and event.is_action_pressed("ui_up"):
 		speed_y = -JUMP_FORCE
+		jump_count += 1
 	pass
 
 func set_direction():
@@ -53,6 +57,8 @@ func _process(delta):
 	speed_x = clamp(speed_x, 0, MAX_SPEED)
 	
 	speed_y += GRAVITY * delta
+	if speed_y > MAX_FALL_SPEED:
+		speed_y = MAX_FALL_SPEED
 	
 	velocity.x = speed_x * delta * direction
 	velocity.y = speed_y * delta
@@ -66,5 +72,6 @@ func _process(delta):
 		var final_movement = normal.slide(movement_reminder)
 		speed_y = normal.slide(Vector2(0, speed_y)).y
 		move(final_movement)
+		jump_count = 0
 		print("Collision")
 	pass
