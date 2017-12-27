ad1df9cd26f0a67ca9714850fabc6b0f7d25b126
Diego Garrido <de.garrido95@gmail.com>
2017-12-26 22:43:23 -0300

    added jump mechanic

--- Scripts/DavidScript.gd
+++ Scripts/DavidScript.gd
@@ -2,15 +2,26 @@ extends KinematicBody2D
 
 var direction = 0
 var input_direction = 0
+var velocity = Vector2()
+var speed_x = 0
+var speed_y = 0
+
 const ACCEL = 250
 const DECEL = 500
 const MAX_SPEED = 150
-var velocity = 0
-var speed = 0
+const JUMP_FORCE = 800
+const GRAVITY = 2000
 
 func _ready():
+	set_process_input(true)
 	set_process(true)
 	pass
+
+func _input(event):
+	if event.is_action_pressed("ui_up"):
+		speed_y = -JUMP_FORCE	
+	pass
+
 func set_direction():
 	if input_direction:
 		direction = input_direction
@@ -21,15 +32,20 @@ func set_direction():
 	else:
 		input_direction = 0
 	pass
+	
 func _process(delta):
 	set_direction()
 	
 	if input_direction:
-		speed += ACCEL * delta
+		speed_x += ACCEL * delta
 	else:
-		speed -= DECEL * delta
+		speed_x -= DECEL * delta
 	
-	speed = clamp(speed, 0, MAX_SPEED)
-	velocity = speed * delta * direction
-	move(Vector2(velocity, 0))
+	speed_x = clamp(speed_x, 0, MAX_SPEED)
+	
+	speed_y += GRAVITY * delta
+	
+	velocity.x = speed_x * delta * direction
+	velocity.y = speed_y * delta
+	move(velocity)
 	pass
